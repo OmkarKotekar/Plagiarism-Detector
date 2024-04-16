a92076c1f7141b7fccd82ca8d3f32ede4bb3f49a
Omkar Kotekar <okotekar@gmail.com>
2024-04-16 23:50:50 +0530

    done with some changes

--- Plagiarism-Detector-master/kmp.py
+++ Plagiarism-Detector-master/kmp.py
@@ -1,28 +1,82 @@
+# from lps import computeLPSArray
+
+# def KMPSearch(pat, text, p):
+
+#     M = len(pat)
+#     N = len(text)
+#     lps = [0]*M
+#     j = 0 
+#     computeLPSArray(pat, M, lps)
+ 
+#     i = 0 
+#     while i < N:
+#         if pat[j].lower() == text[i].lower():
+#             i += 1
+#             j += 1
+ 
+#         if j == M:
+#             p +=1
+#             j = lps[j-1]
+#             break
+
+#         elif i < N and pat[j].lower() != text[i].lower():
+#             if j != 0:
+#                 j = lps[j-1]
+#             else:
+#                 i += 1      
+#     return p
+ 
+# from lps import computeLPSArray
+# import re
+
+# def KMPSearch(pat, text, p):
+#     pat_words = re.sub(r'[^\w\s]', '', pat.lower()).split()  # Remove punctuation and split into words
+#     text_words = re.sub(r'[^\w\s]', '', text.lower()).split()  # Remove punctuation and split into words
+    
+#     M = len(pat_words)
+#     N = len(text_words)
+#     lps = [0] * M
+#     computeLPSArray(pat_words, M, lps)
+#     j = 0 
+#     i = 0 
+#     while i < N:
+#         if pat_words[j] == text_words[i]:
+#             i += 1
+#             j += 1
+#             if j == M:
+#                 p += 1
+#                 j = lps[j-1]
+#         else:
+#             if j != 0:
+#                 j = lps[j-1]
+#             else:
+#                 i += 1      
+#     return p
+
+
+import re
 from lps import computeLPSArray
 
 def KMPSearch(pat, text, p):
+    pat_words = re.findall(r'\w+', pat.lower())  # Split pattern into words
+    text_sentences = re.split(r'[\.\?!]', text.lower())  # Split text into sentences
+    text_words = [re.findall(r'\w+', sentence) for sentence in text_sentences]  # Split each sentence into words
 
-    M = len(pat)
-    N = len(text)
-    lps = [0]*M
+    M = len(pat_words)
+    N = sum(len(sentence) for sentence in text_words)  # Total number of words in text
+    lps = [0] * M
+    computeLPSArray(pat_words, M, lps)
     j = 0 
-    computeLPSArray(pat, M, lps)
- 
-    i = 0 
-    while i < N:
-        if pat[j].lower() == text[i].lower():
-            i += 1
+    for i in range(len(text_words)):
+        k = 0  # Index for pattern words
+        for word in text_words[i]:
+            while k > 0 and pat_words[k] != word:
+                k = lps[k-1]
+            if pat_words[k] == word:
+                k += 1
+            if k == M:
+                p += 1
+                k = lps[k-1]
             j += 1
- 
-        if j == M:
-            p +=1
-            j = lps[j-1]
-            break
-
-        elif i < N and pat[j].lower() != text[i].lower():
-            if j != 0:
-                j = lps[j-1]
-            else:
-                i += 1      
     return p
- 
+
--- Plagiarism-Detector-master/main.py
+++ Plagiarism-Detector-master/main.py
@@ -1,25 +1,85 @@
+# import re
+# from kmp import KMPSearch
+
+# root_file=input("Enter The Name of root file ")
+# plagiarised_file=input("Enter The Name of plagiarised file ")
+  
+# Text = open(root_file,"r")
+# text = Text.read()
+
+# pattern_file = open(plagiarised_file,"r").read()
+# sentences = re.split(r'[\.\?!\r\n]', pattern_file)
+# counter_matched = 0
+# counter_total = 0
+# p=0
+# for pattern in sentences:
+#   pattern = pattern.strip()
+
+#   if len(pattern) > 0:
+#     counter_total +=1
+#     counter_matched += KMPSearch(pattern, text,p)
+          
+# print ("Match percentage = %s%%" % (counter_matched*100/counter_total))
+
+# if(counter_matched*100/counter_total) >= 70 :
+#   print ("The input file appears to be plagiarised. %s%% of its content matches with the file %s." % ((counter_matched*100/counter_total), root_file))  
+
 import re
 from kmp import KMPSearch
 
-root_file=input("Enter The Name of root file ")
-plagiarised_file=input("Enter The Name of plagiarised file ")
-  
-Text = open(root_file,"r")
-text = Text.read()
+def highlight_plagiarised(text, plagiarised_text):
+    highlighted_text = ""
+    for word in text.split():
+        if word in plagiarised_text.split():
+            highlighted_text += "<span style='color:red'>" + word + "</span> "
+        else:
+            highlighted_text += word + " "
+    return highlighted_text
 
-pattern_file = open(plagiarised_file,"r").read()
-sentences = re.split(r'[\.\?!\r\n]', pattern_file)
+root_file = input("Enter The Name of root file: ")
+plagiarised_file = input("Enter The Name of plagiarised file: ")
+
+with open(root_file, "r") as root:
+    text = root.read()
+
+with open(plagiarised_file, "r") as plagiarised:
+    plagiarised_text = plagiarised.read()
+
+sentences = re.split(r'[\.\?!\r\n]', plagiarised_text)
+
 counter_matched = 0
 counter_total = 0
-p=0
+p = 0
+plagiarised_content = ""
+
 for pattern in sentences:
-  pattern = pattern.strip()
+    pattern = pattern.strip()
 
-  if len(pattern) > 0:
-    counter_total +=1
-    counter_matched += KMPSearch(pattern, text,p)
-          
-print ("Match percentage = %s%%" % (counter_matched*100/counter_total))
+    if len(pattern) > 0:
+        counter_total += 1
+        counter_matched += KMPSearch(pattern, text, p)
 
-if(counter_matched*100/counter_total) >= 70 :
-  print ("The input file appears to be plagiarised. %s%% of its content matches with the file %s." % ((counter_matched*100/counter_total), root_file))  
+        if KMPSearch(pattern, text, p) > 0:
+            highlighted = highlight_plagiarised(text, pattern)
+            plagiarised_content += highlighted + "<br>"
+
+match_percentage = (counter_matched * 100) / counter_total
+
+print("\nMatch percentage = %s%%" % match_percentage)
+
+if match_percentage >= 30:
+    print("The input file appears to be plagiarised. %s%% of its content matches with the file %s." % (
+        match_percentage, root_file))
+
+    # Generate Word document
+    report_filename = "plagiarised_report.doc"
+    try:
+        with open(report_filename, "w") as report_file:
+            report_file.write("<html><body>")
+            report_file.write("<p>Plagiarised Text (Highlighted in Red):</p>")
+            report_file.write("<p>" + plagiarised_content + "</p>")
+            report_file.write("<p>Match percentage: %s%%</p>" % match_percentage)
+            report_file.write("</body></html>")
+        print("Word document report generated:", report_filename)
+    except Exception as e:
+        print("Error occurred while creating the Word document:", e)
